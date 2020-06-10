929720aac9a482619b8123972eb2c4647c38a771
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-06-10 21:52:16 +0500

    [2] - Setup simple server

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from ProList 📝");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
