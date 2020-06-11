02aa13093f099f9068f5ec48b62c4af4b874afb2
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-06-11 09:26:38 +0500

    [8] - Connect MongoDB

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "config";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = config.get("port");
@@ -8,6 +9,19 @@ app.get("/", (req, res) => {
   res.send("Hello from ProList 📝");
 })
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-})
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    })
+  } catch (error) {
+    console.log("Sorry but something is wrong ", error.message);
+    process.exit(1);
+  }
+}
+start();
