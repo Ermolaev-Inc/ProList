4f0c3d22257f17da544c1f76831a9484454503c5
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-06-13 22:16:56 +0500

    [12] - Design Auth Component

--- client/src/components/auth/Auth.tsx
+++ client/src/components/auth/Auth.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import classes from "./styles/Auth.module.css";
+import logo from "./img/logo.svg";
 
 export const Auth = () => {
   return(
-    <div>
-      This is Auth Page
+    <div className={classes.window}>
+      <img src={logo} alt="Please wait" className={classes.logo}/>
+      <input type="text" placeholder="Username" className={classes.login}/>
+      <input type="text" placeholder="Password" className={classes.password}/>
+      <div className={classes.buttons}>
+        <button className={classes.signup_btn}>Sign up</button>
+        <button className={classes.login_btn}>Login</button>
+      </div>
+      
     </div>
   )
 }
