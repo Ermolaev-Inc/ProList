c08ad5ef1b5c93902f97325c10b6da500bdb1b61
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-06-13 23:02:26 +0500

    [13] - Add login and password handlers

--- client/src/components/auth/Auth.tsx
+++ client/src/components/auth/Auth.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./styles/Auth.module.css";
 import logo from "./img/logo.svg";
 
 export const Auth = () => {
+  const [login, setLogin] = useState();
+  const [password, setPassword] = useState();
+  const loginHandler = (event: any) => {
+    setLogin(event.target.value);
+  }
+  const passwordHandler = (event: any) => {
+    setPassword(event.targer.value);
+  }
   return(
     <div className={classes.window}>
       <img src={logo} alt="Please wait" className={classes.logo}/>
-      <input type="text" placeholder="Username" className={classes.login}/>
-      <input type="text" placeholder="Password" className={classes.password}/>
+      <input type="text" placeholder="Username" name="login" onChange={loginHandler} className={classes.login}/>
+      <input type="text" placeholder="Password" name="password" onChange={passwordHandler} className={classes.password}/>
       <div className={classes.buttons}>
         <button className={classes.signup_btn}>Sign up</button>
         <button className={classes.login_btn}>Login</button>
