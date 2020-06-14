5cc61ba78f11ecd5d34ef73008d0088f36bf3622
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-06-14 12:52:17 +0500

    [16] - Register logic

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,14 +1,39 @@
 import { Router } from "express";
-
+import { check, validationResult } from "express-validator";
+import { User } from "../models/User";
+import bcrypt from "bcryptjs";
 const router = Router();
 
 router.post(
   "/register",
-  async (req, res) => {
-    res.status(201).json({ message: "Ye boy" });
-    console.log(req.body);
+  [
+    check("login", "Min - 3 characters").isLength({ min: 3 }),
+    check("password", "Min - 6 characters").isLength({ min: 6 })
+  ],
+  async (req: any, res: any) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Min - 6 characters"
+        })
+      }
+      const {login, password} = req.body;
+      const candidate = await User.findOne({ login });
+      if (candidate) {
+        return res.status(400).json({ message: "This username is already registered" });
+      }
+      console.log("lol")
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ login, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: "The user is created" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Try again :(" });
+    }
   }
 )
-//router.get("/register", async (req, res) => {res.json({ message: "lol" })})
 
 module.exports = router;
