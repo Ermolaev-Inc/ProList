67ad7909b6f90d2eb77942f0d1adb8c46d3dfd2b
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-07-11 14:04:03 +0500

    [U21 - 6] - Parsing back data

--- client/src/components/app/AppContainer.tsx
+++ client/src/components/app/AppContainer.tsx
@@ -2,11 +2,12 @@ import React, { useCallback, useState, useEffect, useContext } from "react";
 import { useHttp } from "../../hooks/http.hook";
 import classes from "./styles/AppContainer.module.css";
 import { AuthContext } from "../../context/AuthContext";
+import { ProjectsRendering } from "./ProjectsRendering";
 
 export const AppContainer = (props: any) => {
   const request: Function = useHttp();
   const token = useContext(AuthContext);
-  let [data, setData] = useState([]);
+  let [data, setData]: [any, Function] = useState(undefined);
   const fetch = useCallback(async () => {
     try {
       const dataFetched = await request("/api/data", "GET", null, {
@@ -20,10 +21,17 @@ export const AppContainer = (props: any) => {
   useEffect(() => {
     fetch();
   }, [fetch])
-  
+  let [projects, setProjects] = useState("Lol");
+  useEffect(() => {
+    if (data != undefined) {
+      console.log(data);
+      
+      setProjects(data.default.map((element: { name: string; }) => <ProjectsRendering projectName={element.name}/>));
+    }
+  }, [data])
   return(
     <div className={classes.channelForm}>
-      Hello
+      {projects}
     </div>
   )
 }
---
+++ client/src/components/app/ProjectsRendering.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ProjectsRendering = (props: any) => {
+  
+  return(
+    <>
+      {props.projectName}
+    </>
+  )
+}
