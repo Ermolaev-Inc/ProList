1e277941db212f2fca3049e9f549d28e3c7b0032
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-07-26 16:43:16 +0500

    [23] - Create template for creating project

--- routes/create.routes.ts
+++ routes/create.routes.ts
@@ -1,7 +1,24 @@
 import { Router } from "express";
 import { User } from "../models/User";
-import { ILocalStorageData, IUser } from "../interfaces";
 const router = Router();
+
+router.post(
+  "/project",
+  async (req: any, res: any) => {
+    try {
+      const userData = req.body;
+      const userId = userData.authInfo.userId;
+      const user = await User.update({ _id: userId }, {$push: {personalChannel: {projectName:"New Project"}}}, {upsert: false});
+      if (!user) {
+        return res.status(400).json({ message: "This user does not exis" });
+      }
+      await user.save();  
+      res.status(201).json({ message: "Success" });
+    } catch (error) {
+      res.status(500).json({ message: "Something is wrong :(" });
+    }
+  }
+)
 
 /* router.post(
   "/todo",
