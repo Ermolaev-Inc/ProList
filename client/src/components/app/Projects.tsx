f6d0f6bd0aede5326e03e9b41ee6458a0aa4c74e
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-01 14:11:15 +0500

    [27] - Start injecting logic of deleting projects

--- client/src/components/app/Projects.tsx
+++ client/src/components/app/Projects.tsx
@@ -3,14 +3,24 @@ import classes from "./styles/AppContainer.module.css";
 import { IPropsProjects } from "../../interfaces";
 
 export const Projects = (props: IPropsProjects) => {
-  let [isSelected, setSelected] = useState(false);
-  const selectProject = () => {
+  let [isSelected, setSelected]: [boolean, Function] = useState(false);
+  let [isContextMenu, setContextMenu]: [boolean, Function] = useState(false);
+  const selectProject = (): void => {
     setSelected(!isSelected);
+  }
+  const showContextMenu = (event: any): void => {
+    //TODO: Context Menu (#27)
+    event.preventDefault();
+    setContextMenu(!isContextMenu);
   }
   return(
     <div className={classes.project}>
       <div className={isSelected ? classes.selectedForm : classes.notSelectedForm}>
-        <li onClick={selectProject} className={isSelected ? classes.selectedProject : classes.notSelectedProject}>{props.projectName}</li>
+        <li onClick={selectProject} 
+            onContextMenu={showContextMenu} 
+            className={isSelected ? classes.selectedProject : classes.notSelectedProject}>
+              <span className={isContextMenu ? classes.contextMenu : ""}>{props.projectName}</span>
+        </li>
       </div>
     </div>
   )
