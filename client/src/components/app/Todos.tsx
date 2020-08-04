1070bf83b93c948270c9be5639cb86aa8184278a
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-05 00:47:49 +0500

    [30] - Add identifyStatus function

--- client/src/components/app/Todos.tsx
+++ client/src/components/app/Todos.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { IPropsTodos } from "../../interfaces";
+import classes from "./styles/Todos.module.css";
+import { Status } from "../../statuses";
 
 export const Todos = (todo: IPropsTodos) => {
-  
+  let [statusCircle, setStatusCircle] = useState(todo.status);
+  const identifyStatus = (): string => {
+    // TODO: use case
+    if (statusCircle === Status.COMPLETED) {
+      return classes.completed;
+    } else if (statusCircle === Status.IN_PROGRESS) {
+      return classes.inProgress;
+    } else {
+      return classes.notStarted;
+    }
+  }
   return(
     <li>
+      <div className={identifyStatus()}></div>
       {todo.name}
     </li>
   )
---
+++ client/src/statuses.ts
@@ -0,0 +1,5 @@
+export enum Status {
+  COMPLETED = "COMPLETED",
+  IN_PROGRESS = "IN_PROGRESS",
+  NOT_STARTED = "NOT_STARTED",
+}
