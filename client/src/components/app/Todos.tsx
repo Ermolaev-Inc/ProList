0fbfebac7e99ea56cbd156a8735a5ce9357ccb66
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-06 18:30:14 +0500

    Refactoring if -> switch in Todos

--- client/src/components/app/Todos.tsx
+++ client/src/components/app/Todos.tsx
@@ -6,13 +6,15 @@ import { Status } from "../../statuses";
 export const Todos = (todo: IPropsTodos) => {
   let [statusCircle, setStatusCircle] = useState(todo.status);
   const identifyStatus = (): string => {
-    // TODO: use case
-    if (statusCircle === Status.COMPLETED) {
-      return classes.completed;
-    } else if (statusCircle === Status.IN_PROGRESS) {
-      return classes.inProgress;
-    } else {
-      return classes.notStarted;
+    switch (statusCircle) {
+      case Status.COMPLETED:
+        return classes.completed;
+      case Status.IN_PROGRESS:
+        return classes.inProgress;
+      case Status.NOT_STARTED:
+        return classes.notStarted;
+      default:
+        return "Error";
     }
   }
   return(
