85de863e8897681f2514adc0b227baf382bd801a
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-18 00:23:07 +0500

    [36] - Add logout function

--- client/src/components/settings/SettingsContainer.tsx
+++ client/src/components/settings/SettingsContainer.tsx
@@ -4,17 +4,20 @@ import { ThemeContext, Theme } from "../../context/ThemeContext";
 import closeButtonLight from "./img/closeButtonLight.svg";
 import closeButtonDark from "./img/closeButtonDark.svg";
 import { IPropsSettingsContainer } from "../../interfaces";
+import { useLogout } from "../../hooks/logout.hook";
 
 export const SettingsContainer = (props: IPropsSettingsContainer) => {
   const themeContext = useContext(ThemeContext);
   const theme = themeContext.theme;
   const changeTheme = themeContext.changeTheme;
+  const {logout} = useLogout();
   return(
     <div className={theme === Theme.LIGHT ? classes.wrapper : classes.wrapperDark}>
       <div className={classes.titlesSectionWrapper}>
         <div className={classes.titlesSection}>
           <span>Create a new channel</span>
           <span onClick={changeTheme}>Change theme</span>
+          <span className={classes.logoutButton} onClick={logout} >Logout</span>
         </div>
       </div>
       <img className={classes.closeButton} src={theme === Theme.LIGHT ? closeButtonLight : closeButtonDark} alt="" onClick={props.closeSettings} />
---
+++ client/src/hooks/logout.hook.ts
@@ -0,0 +1,7 @@
+export const useLogout = () => {
+  const logout = () => {
+    localStorage.removeItem("userData");
+    window.location.reload();
+  }
+  return { logout }
+}
