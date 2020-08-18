62c25ff9b22f4267444a3b5d193a988d5e63369d
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-18 15:14:25 +0500

    [32] - Create CreateChannelTemplate

---
+++ client/src/components/settings/CreateChannelTemplate.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CreateChannelTemplate = () => {
+  return(
+    <div>
+      
+    </div>
+  )
+}
--- client/src/components/settings/SettingsContainer.tsx
+++ client/src/components/settings/SettingsContainer.tsx
@@ -1,25 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./css/SettingsContainer.module.css";
 import { ThemeContext, Theme } from "../../context/ThemeContext";
 import closeButtonLight from "./img/closeButtonLight.svg";
 import closeButtonDark from "./img/closeButtonDark.svg";
 import { IPropsSettingsContainer } from "../../interfaces";
 import { useLogout } from "../../hooks/logout.hook";
+import { CreateChannelTemplate } from "./CreateChannelTemplate";
 
 export const SettingsContainer = (props: IPropsSettingsContainer) => {
   const themeContext = useContext(ThemeContext);
   const theme = themeContext.theme;
   const changeTheme = themeContext.changeTheme;
   const {logout} = useLogout();
+  let [isChannelTemplate, setChannelTemplate] = useState(false);
+  const showCreateChannelTemplate = () => {
+    setChannelTemplate(true);
+  }
   return(
     <div className={theme === Theme.LIGHT ? classes.wrapper : classes.wrapperDark}>
       <div className={classes.titlesSectionWrapper}>
         <div className={classes.titlesSection}>
-          <span>Create a new channel</span>
+          <span onClick={showCreateChannelTemplate}>Create a channel</span>
           <span onClick={changeTheme}>Change theme</span>
           <span className={classes.logoutButton} onClick={logout} >Logout</span>
         </div>
       </div>
+      {isChannelTemplate && <CreateChannelTemplate />}
       <img className={classes.closeButton} src={theme === Theme.LIGHT ? closeButtonLight : closeButtonDark} alt="" onClick={props.closeSettings} />
     </div>
   )
