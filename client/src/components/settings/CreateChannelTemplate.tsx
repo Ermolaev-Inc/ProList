7142a801bd19aae3cb37283253ab846948393008
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-20 22:34:56 +0500

    [32] - Update Settings UI

--- client/src/components/settings/CreateChannelTemplate.tsx
+++ client/src/components/settings/CreateChannelTemplate.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const CreateChannelTemplate = () => {
   return(
     <div>
-
+      <input type="text"/>
+      <input type="text"/>
+      <div></div>
     </div>
   )
 }
