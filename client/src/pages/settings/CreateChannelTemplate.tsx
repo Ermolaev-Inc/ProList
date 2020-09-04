011f9a4b6309ea3b275c5c82c07c8404b41a4fc6
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-09-04 18:03:16 +0500

    Add CreateChannel client side logic

--- client/src/pages/settings/CreateChannelTemplate.tsx
+++ client/src/pages/settings/CreateChannelTemplate.tsx
@@ -1,16 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import s from "./sass/CreateChannelTemplate.module.sass";
 import { ThemeContext, Theme } from "../../context/ThemeContext";
 import { NeonButton } from "../../components/NeonButton/NeonButton";
+import { useHttp } from "../../hooks/http.hook";
 
 export const CreateChannelTemplate = () => {
+  const request: Function = useHttp();
   const themeContext = useContext(ThemeContext);
   const theme = themeContext.theme;
+
+  const [channelName, setChannelName]: [string, Function] = useState("");
+  const [password, setPassword]: [string, Function] = useState("");
+
+  const channelNameInputHandler = (event: any): void => {
+    setChannelName(event.target.value);
+  }
+  const passwordInputHandler = (event: any): void => {
+    setPassword(event.target.value);
+  }
+  const createChannelHandler = () => {
+    try {
+      const data = request("/api/channels/create", "POST", {channelName, password});
+      // TODO: error/success UI handler 
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
   return(
     <div className={theme === Theme.LIGHT ? s.light : s.dark}>
-      <input type="text" className={s.channelName} placeholder="Channel name"/>
-      <input type="password" className={s.channelName} placeholder="Password"/>
-      <NeonButton title="Create" className={s.createButton} />
+      <input type="text" className={s.channelName} placeholder="Channel name" onChange={channelNameInputHandler}/>
+      <input type="password" className={s.channelName} placeholder="Password" onChange={passwordInputHandler}/>
+      <NeonButton title="Create" className={s.createButton} onClick={createChannelHandler}/>
     </div>
   )
 }
