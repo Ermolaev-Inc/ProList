200a48bd97af7afe8ae76ae844aeb71e857eb30c
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-09-13 15:49:07 +0500

    Create SettingsButton Component

---
+++ client/src/components/SettingsButton/interfaces.ts
@@ -0,0 +1,5 @@
+export interface SettingsButtonProps {
+  color?: string;
+  onClick?(args: any): any;
+  [name: string]: any;
+}
--- client/src/pages/app/ChannelsSection/ChannelsContainer.tsx
+++ client/src/pages/app/ChannelsSection/ChannelsContainer.tsx
@@ -1,26 +1,18 @@
 import React, { useContext, useState } from "react";
-import classes from "../sass/ChannelsContainer.module.sass";
+import s from "../sass/ChannelsContainer.module.sass";
 import { ThemeContext, Theme } from "../../../context/ThemeContext";
 import { useHttp } from "../../../hooks/http.hook";
-import { SettingsButton } from "./SettingsButton";
+import { SettingsButton } from "../../../components/SettingsButton/SettingsButton";
 import { IPropsChannelsContainer } from "../../../interfaces";
 
 export const ChannelsContainer = (props: IPropsChannelsContainer) => {
   const request: Function = useHttp();
   const themeContext = useContext(ThemeContext);
-  const theme = themeContext.theme;
-  let [isSettings, setSettings] = useState(false);
-  const showSettings = (): void => {
-    setSettings(true);
-  }
-  //TODO
-  //const createChannel = async () => {
-    //await request("/api/channels/create", "POST", null, {});
-  //}
+
   return(
-    <div className={theme === Theme.LIGHT ? classes.channelsWrapper : classes.channelsWrapperDark}>
-      <div className={classes.default}></div>
-      <SettingsButton showSettings={props.showSettings}/>
+    <div className={themeContext.theme === Theme.LIGHT ? s.channelsWrapper : s.channelsWrapperDark}>
+      <div className={s.default}></div>
+      <SettingsButton onClick={props.showSettings} color={themeContext.theme === Theme.LIGHT ? "#000000" : "#ffffff"}/>
     </div>
   )
 }
