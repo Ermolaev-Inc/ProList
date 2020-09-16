d48e2f170d5e9cd3ed165b7b1715323c74cbb5fb
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-09-16 22:45:16 +0500

    Fix onClick working in AddButton Component

--- client/src/components/AddButton/AddButton.tsx
+++ client/src/components/AddButton/AddButton.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { AddButtonProps } from "./interfaces";
 
@@ -7,7 +8,7 @@ export const AddButton: React.FC<AddButtonProps> = ({
   ...props
 }) => {
   return(
-    <svg {...props} style={{cursor: "pointer"}} width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg onClick={onClick} {...props} style={{cursor: "pointer"}} width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="11" width="4" height="26" rx="2" fill={color}/>
     <rect y="15" width="4" height="26" rx="2" transform="rotate(-90 0 15)" fill={color}/>
     </svg>
